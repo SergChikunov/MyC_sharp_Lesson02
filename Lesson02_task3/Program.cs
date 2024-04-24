2c77ad2f3cf1c34d974325c03ec38bde6860fad7
Sergey Chikunov <consull@outlook.com>
2024-04-24 22:10:54 +0500

    Csharp массивы. Урок 2, задание 3, поиск четных элементов массива

---
+++ Lesson02_task3/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = 10;
+int[] array = { 1, 5, 3, 7, 9, 35, 1, 3, 11, 5 };
+int i = 0;
+int count = 0;
+
+Console.WriteLine("Четные элементы массива имеют такие значения: ");
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+        count++;
+    }
+    i++;
+}
+
+if(count == 0)
+{
+    Console.Write("в массиве нет четных элементов");
+}
