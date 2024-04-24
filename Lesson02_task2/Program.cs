bee28d467ad0a37cb559d4537d0517ef86b89528
Sergey Chikunov <consull@outlook.com>
2024-04-24 21:42:09 +0500

    Csharp массивы. Урок 2, задача 2

---
+++ Lesson02_task2/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+Console.Write("Массив заполнен следующими значениями элементов: ");
+while (i < n)
+{
+    arr[i] = i + 1;
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
