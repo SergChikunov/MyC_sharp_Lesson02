9fb66609c7580f8d22374e94c8992e4e129681f0
Sergey Chikunov <consull@outlook.com>
2024-04-27 21:42:11 +0500

    Csharp. Функции. Урок 3. Задание 2. Функция вывода квадрата чисел от 1 до N в консоль

---
+++ Lesson03_task2/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintSquares(int limit)
+{
+	int i = 1;
+	while(i <= limit)
+	{
+		Console.Write($"{i * i} ");
+        i++;
+	}
+}
+PrintSquares(5);
+Console.Write("\n");
+PrintSquares(10);
+Console.Write("\n");
+PrintSquares(15);
