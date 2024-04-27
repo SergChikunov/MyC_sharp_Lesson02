7ab8f030082cfb7201b9bf98d6a6ae581d877d92
Sergey Chikunov <consull@outlook.com>
2024-04-27 21:25:41 +0500

    Csharp Урок 3 Функции Задание знакомство с функциями, первая функция

---
+++ Lesson03_task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Урок_3. Функции
+
+//Задание_1
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    int numenator = a * b;
+    double denumenator = c + d;     // чтобы результат вернулся в вещественном виде, надо чтобы одна переменная имела вещественный тип данных
+    double result = numenator / denumenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine("Результат вычисления выражения: " + result);
+Console.WriteLine("Результат вычисления выражения: " + CalculateFormula(1, 2, 3, 4)); //так тоже работает!!!
