0b7e0a005138e96e58b84ea04a9a7569048a6342
Sergey Chikunov <consull@outlook.com>
2024-05-15 22:56:50 +0500

    Занятие 4. Задание 3. Задать в консоли строку состояющую из букв и цифр, а затем сформировать из нее строку только из букв этой строки

---
+++ Lesson04_Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Считать с консоли строку, состоящую из цифр и латинских букв.
+Сформировать новую строку, состоящую из букв исходной строки.
+123passw0rd*/
+
+string GetLettersFronString(string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+Console.WriteLine("Введите исходную строку и нажмите Enter...");
+string str = Console.ReadLine();
+string result = GetLettersFronString(str);
+Console.WriteLine(result);
